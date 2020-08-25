6f0591313ce53dc917d315e920cc8f5424708cbc
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-25 21:51:18 +0300

    Added instance for those using simulator and those using expo devices

--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://172.20.10.3:8000",
+  //Anas
+  // baseURL: "http://172.20.10.3:8000",
+
+  //Yousif & Sarah
+  baseURL: "http://localhost:8000",
+
   // baseURL: "https://gentle-shore-83533.herokuapp.com",
 });
 
