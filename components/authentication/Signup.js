b6615070f0d91df7d0384a30f94297bfc54db0ec
Sarah <S00038939@auk.edu.kw>
2020-08-26 15:58:34 +0300

    created signup function

---
+++ components/authentication/Signup.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+//Stores
+import authStore from "../../stores/authStore";
+
+//Styles
+import {
+  AuthTitle,
+  AuthTextInput,
+  AuthButton,
+  AuthButtonText,
+  AuthOther,
+  BackgroundImage,
+} from "./styles";
+
+const Signup = ({ navigation }) => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async () => {
+    await authStore.signup(user);
+    if (authStore.user) navigation.navigate("Trips");
+  };
+  return (
+    <BackgroundImage
+      source={{
+        uri:
+          "https://i.pinimg.com/originals/81/fd/00/81fd00d4f4b7a7f5fe3049fbb4b668bc.jpg",
+      }}
+    >
+      <AuthTitle>Sign up</AuthTitle>
+      <AuthTextInput
+        onChangeText={(firstName) => setUser({ ...user, firstName })}
+        placeholder="First Name"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={false}
+        autoCapitalize="none"
+      />
+      <AuthTextInput
+        onChangeText={(lastName) => setUser({ ...user, lastName })}
+        placeholder="Last Name"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={false}
+        autoCapitalize="none"
+      />
+      <AuthTextInput
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="Email"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={false}
+        autoCapitalize="none"
+      />
+
+      <AuthTextInput
+        onChangeText={(username) => setUser({ ...user, username })}
+        placeholder="Username"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={false}
+        autoCapitalize="none"
+      />
+      <AuthTextInput
+        onChangeText={(password) => setUser({ ...user, password })}
+        placeholder="Password"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={true}
+      />
+      <AuthButton onPress={handleSubmit}>
+        <AuthButtonText>Sign up</AuthButtonText>
+      </AuthButton>
+      <AuthOther onPress={() => navigation.navigate("Signin")}>
+        Already have an account? Sign in.
+      </AuthOther>
+    </BackgroundImage>
+  );
+};
+
+export default Signup;
