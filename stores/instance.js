0117ff64b2f0fb6a8b3c6f0d228c953e0fc3c85d
Sarah <S00038939@auk.edu.kw>
2020-08-26 16:00:39 +0300

    changed base url

--- stores/instance.js
+++ stores/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://172.20.10.3:8000",
+  baseURL: "http://localhost:8000", // Sarah's base url
+  //  baseURL: "http://172.20.10.3:8000", ; Anas' base url
   // baseURL: "https://gentle-shore-83533.herokuapp.com",
 });
 
