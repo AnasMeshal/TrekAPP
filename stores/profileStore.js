31c59e4af801ceab45430820c3e87beb7816f68a
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-29 19:51:37 +0300

    added 'findnotmyprofile' function to get the profile of a trip from the backend

--- stores/profileStore.js
+++ stores/profileStore.js
@@ -1,17 +1,19 @@
 //React
 import { decorate, observable } from "mobx";
 import instance from "./instance";
-import authStore from "./authStore";
 
 class ProfileStore {
+  loading = true;
   profile = [];
-
+  notMyProfile = [];
+  //TODO SET LOADING TO FALSE ONCE YOU OPEN THE PROFILE
+  //TODO: IF SIGNED IN FOR TOO LONG DELETE TOKEN OR ELSE ERRORS
   fetchProfile = async (user) => {
     try {
       const res = await instance.get("/profiles", user);
       this.profile = res.data;
     } catch (error) {
-      console.log(error);
+      console.error("ProfileStore -> fetchProfile -> error", error);
     }
   };
 
@@ -20,13 +22,26 @@ class ProfileStore {
       await instance.put("/profiles", updatedProfile);
       this.profile = updatedProfile;
     } catch (error) {
-      console.log(error);
+      console.error("ProfileStore -> profileUpdate -> error", error);
+    }
+  };
+
+  findNotMyProfile = async (userId) => {
+    try {
+      const res = await instance.post("/profiles", userId);
+      this.notMyProfile = res.data;
+      this.loading = false;
+      return this.notMyProfile;
+    } catch (error) {
+      console.error("ProfileStore -> notMyProfile -> error", error);
     }
   };
 }
 
 decorate(ProfileStore, {
   profile: observable,
+  notMyProfile: observable,
+  loading: observable,
 });
 
 const profileStore = new ProfileStore();
