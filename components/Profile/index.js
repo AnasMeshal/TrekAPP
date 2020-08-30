dddd516a53066acf5dc865a436ef34b08031ea97
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-08-30 21:12:43 +0300

    changed the profile and style it so when the user logout it shows log in button and extra stuff

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 
+//Buttons
+import AddTripButton from "../buttons/AddTripButton";
+
 //Components
+import MyTripList from "../MyTripList";
 
 //Stores
 import profileStore from "../../stores/profileStore";
+import authStore from "../../stores/authStore";
 
 //Styles
-import { ProfileImage, ProfileName, ProfileBio } from "./styles";
+import {
+  ProfileImage,
+  ProfileName,
+  ProfileBio,
+  SignInOrSignUpButton,
+  StyledText,
+} from "./styles";
+
+import {
+  CenterButton,
+  SignInButtonStyled,
+  SignUpButtonStyled,
+  BackgroundImage,
+  Title,
+  SkipButtonStyled,
+  SkipTextStyled,
+} from "./styles";
 import { ScrollView } from "react-native";
-import MyTripList from "../MyTripList";
-import authStore from "../../stores/authStore";
 import { Text } from "native-base";
 
 const Profile = ({ navigation }) => {
@@ -25,15 +44,16 @@ const Profile = ({ navigation }) => {
 
   if (authStore.user) {
     return (
-      <ScrollView>
-        <ProfileImage
-          source={{
-            uri:
-              "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png",
-          }}
-        />
+      <>
+        <ScrollView>
+          <ProfileImage
+            source={{
+              uri:
+                "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png",
+            }}
+          />
 
-        {/* TODO profile name
+          {/* TODO profile name
        <ProfileName
         maxLength={40}
         blurOnSubmit={true}
@@ -47,20 +67,48 @@ const Profile = ({ navigation }) => {
           await tripStore.tripUpdate(updatedProfile);
         }}
       /> */}
-        <ProfileBio
-          multiline={true}
-          placeholder={profile.bio}
-          placeholderTextColor="grey"
-          onChangeText={(bio) => setUpdatedProfile({ ...updatedProfile, bio })}
-          onEndEditing={async () => {
-            await profileStore.profileUpdate(updatedProfile);
-          }}
-        />
-        <MyTripList isProfile navigation={navigation} />
-      </ScrollView>
+          <ProfileBio
+            multiline={true}
+            placeholder={profile.bio}
+            placeholderTextColor="grey"
+            onChangeText={(bio) =>
+              setUpdatedProfile({ ...updatedProfile, bio })
+            }
+            onEndEditing={async () => {
+              await profileStore.profileUpdate(updatedProfile);
+            }}
+          />
+          <MyTripList isProfile navigation={navigation} />
+        </ScrollView>
+        <AddTripButton />
+      </>
     );
   } else {
-    return <Text>Sign in</Text>;
+    return (
+      <>
+        <StyledText>
+          You need to sign in or sign up to view your profile
+        </StyledText>
+        <SignInOrSignUpButton onPress={() => navigation.navigate("Signin")}>
+          <Text>Sign in</Text>
+        </SignInOrSignUpButton>
+        {/* <BackgroundImage
+          source={{
+            uri:
+              "https://i.pinimg.com/originals/81/fd/00/81fd00d4f4b7a7f5fe3049fbb4b668bc.jpg",
+          }}
+        >
+          <Title>Welcome to Trek</Title>
+
+          <SignInButtonStyled onPress={() => navigation.replace("Signin")}>
+            <Text>Sign in</Text>
+          </SignInButtonStyled>
+          <SignUpButtonStyled onPress={() => navigation.replace("Signup")}>
+            <Text>Sign up</Text>
+          </SignUpButtonStyled>
+        </BackgroundImage> */}
+      </>
+    );
   }
 };
 
