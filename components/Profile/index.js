645a02944483539032ce6f7e24fd4107d563dfdd
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-31 16:45:04 +0300

    markdown placeholder text

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -12,6 +12,7 @@ import { ScrollView } from "react-native";
 import MyTripList from "../MyTripList";
 import authStore from "../../stores/authStore";
 import { Text } from "native-base";
+import Markdown from "react-native-simple-markdown";
 
 const Profile = ({ navigation }) => {
   const { profile } = profileStore;
@@ -57,6 +58,20 @@ const Profile = ({ navigation }) => {
             await profileStore.profileUpdate(updatedProfile);
           }}
         />
+        {/* <Markdown>
+          #Markdown in react-native is so cool! {"\n\n"}
+          You can **emphasize** what you want, or just _suggest it_ 😏…{"\n"}
+          You can even [**link your
+          website**](https://twitter.com/Charles_Mangwa) or if you prefer:
+          [email somebody](mailto:email@somebody.com){"\n"}
+          Spice it up with some GIFs 💃: ![Some
+          GIF](https://media.giphy.com/media/dkGhBWE3SyzXW/giphy.gif){"\n"}
+          And even add a cool video 😎!{"\n"}
+          [![A cool video from
+          YT](https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg)](http://www.youtube.com/watch?v=dQw4w9WgXcQ)
+          [![Another one from
+          Vimeo](https://i.vimeocdn.com/video/399486266_640.jpg)](https://vimeo.com/57580368)
+        </Markdown> */}
         <MyTripList isProfile navigation={navigation} />
       </ScrollView>
     );
