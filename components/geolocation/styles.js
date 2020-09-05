048f6e3613abb832f85e0d099cc78aea3384256d
Sarah <S00038939@auk.edu.kw>
2020-09-05 17:35:13 +0300

    Added styling to maps component

---
+++ components/geolocation/styles.js
@@ -0,0 +1,14 @@
+// Styled components
+import styled from "styled-components";
+import { Button } from "native-base";
+
+export const MapButton = styled(Button)`
+  background-color: #4285f4; /* Google Blue*/
+  border: none;
+  color: white;
+  text-decoration: none;
+  font-size: 16px;
+  align-self: center;
+  position: absolute;
+  bottom: 0;
+`;
